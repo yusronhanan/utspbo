242c26ef7725c6ed738ea0d7e7fcc85d99ee5cfa
yusronhanan <yusronzain@gmail.com>
2017-03-14 09:48:00 +0700

    sudah selesai

---
+++ src/testcrud/pkg2/file_koneksi.java
@@ -0,0 +1,31 @@
+package testcrud.pkg2;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in
+Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yusron
+ */
+public class file_koneksi {
+
+    private static Connection koneksi;
+    public static Connection GetConnection() throws SQLException
+{
+        if (koneksi == null) {
+            Driver driver = new Driver();
+            koneksi = (Connection)
+            DriverManager.getConnection("jdbc:mysql://localhost:3306/sekolah?zeroDateTimeBehavior=convertToNull", "root", ""); 
+        } 
+        return koneksi;
+    }
+    }
+ 
